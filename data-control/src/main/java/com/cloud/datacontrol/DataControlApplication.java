e8cd769542af19d80c64790c5df0736ce79fa749
zuyunbo <460895202@qq.com>
2021-01-25 16:00:34 +0800

    祖云波

---
+++ data-control/src/main/java/com/cloud/datacontrol/DataControlApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.datacontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataControlApplication.class, args);
+    }
+
+}
